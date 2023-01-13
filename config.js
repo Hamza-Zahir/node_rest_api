33a1a28a1b7d2facc77db3f9491e1de24bff3669
Hamza-Zahir <hamza19zahir98@gmail.com>
2023-01-13 23:24:02 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  MONGODB_URL: process.env.MONGODB_URL,
+};
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
